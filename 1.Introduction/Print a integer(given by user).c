410cb0003a1199d5d75d6ce33c1e7864a70ea760
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2020-11-19 19:28:29 +0530

    Add files via upload

---
+++ 1.Introduction/Print a integer(given by user).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+    int number;
+
+    printf("Enter the Number: ");
+    //reads and stores the value in number
+    scanf("%d",&number);
+
+    //Print the value
+    printf("You entered: %d",number);
+}
